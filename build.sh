e0ac308631d613703d510e2cf6d16e5a5ba95454
Maximilian Mehnert <maximilian.mehnert@gmx.de>
2011-02-22 21:07:42 +0100

    create decks dir

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ rm -rf ankimini/libanki/.git
 
 cp -a /usr/share/pyshared/simplejson ankimini/
 cp -a /usr/share/pyshared/sqlalchemy ankimini/
+mkdir ankimini/decks
 
 
 cp README.windows-mobile.txt ankimini
